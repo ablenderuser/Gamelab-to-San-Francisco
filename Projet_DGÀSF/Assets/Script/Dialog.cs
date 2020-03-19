42e67b33b4a98ed9f41b6d6a598880594103d2fa
root <root@Sylvain-GEISER.localdomain>
2020-03-19 17:36:12 +0100

    Système de dialogue ajouté

---
+++ Projet_DGÀSF/Assets/Script/Dialog.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+// required for display in unity Inspector tab (as it is not a MonoBehavior)
+
+public class Dialog
+{
+    [TextArea(1, 5)] // better input field in Unity Inspector
+    public string m_Sentence;
+    public string[] m_Choices;
+    public Dialog[] m_NextSentences;
+}
