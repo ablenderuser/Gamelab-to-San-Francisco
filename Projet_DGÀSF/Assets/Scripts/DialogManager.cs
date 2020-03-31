b2e92637d3283b89f0328c182334cdc61ff162f4
root <root@Sylvain-GEISER.localdomain>
2020-03-31 16:04:38 +0200

    Slice

---
+++ Projet_DGÀSF/Assets/Scripts/DialogManager.cs
@@ -0,0 +1,224 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public GameObject m_DialogBoxPrefab;
+
+    private GameObject m_DialogBox;
+    private GameObject m_DialogTextUI;
+    private Text m_DialogText;
+
+    public GameObject m_ChoiceButtonPrefab;
+
+    private GameObject m_CanvasFirstChoiceButton;
+    private GameObject m_CanvasSecondChoiceButton;
+    private GameObject m_CanvasThirdChoiceButton;
+    private GameObject m_CanvasFinishDialogButton;
+
+    private GameObject m_FirstChoiceButtonUI;
+    private GameObject m_SecondChoiceButtonUI;
+    private GameObject m_ThirdChoiceButtonUI;
+    private GameObject m_FinishDialogButtonUI;
+
+    private Button m_FirstChoiceButton;
+    private Button m_SecondChoiceButton;
+    private Button m_ThirdChoiceButton;
+    private Button m_FinishDialogButton;
+
+    private Text m_FirstChoiceText;
+    private Text m_SecondChoiceText;
+    private Text m_ThirdChoiceText;
+    private Text m_FinishDialogText;
+
+    private Dialog m_Dialog;
+
+    public Vector3 m_Decal1;
+    public Vector3 m_Decal2;
+    public Vector3 m_Decal3;
+    public Vector3 m_DecalF;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void StartDialog(Dialog dialog)
+    {
+        m_Dialog = dialog;
+
+        m_DialogBox = Instantiate(m_DialogBoxPrefab, transform.position, transform.rotation);
+
+        m_DialogTextUI = GameObject.Find("DialogText");
+        m_DialogText = m_DialogTextUI.GetComponent<Text>();
+        m_DialogText.text = dialog.m_Sentence;
+
+        if (m_Dialog.m_Choices[0] != "")
+        {
+            m_CanvasFirstChoiceButton = Instantiate(m_ChoiceButtonPrefab, transform.position, transform.rotation);
+            m_FirstChoiceButtonUI = GameObject.Find("ChoiceButton");
+            m_FirstChoiceButtonUI.name = "FirstChoiceButton";
+            m_FirstChoiceButtonUI.transform.Translate(m_Decal1);
+            m_FirstChoiceButton = m_FirstChoiceButtonUI.GetComponent<Button>();
+            m_FirstChoiceButton.onClick.AddListener(FirstChoiceDialog);
+
+            GameObject m_FirstChoiceTextUI = GameObject.Find("ChoiceText");
+            m_FirstChoiceTextUI.name = "FirstChoiceText";
+            m_FirstChoiceText = m_FirstChoiceTextUI.GetComponent<Text>();
+            m_FirstChoiceText.text = m_Dialog.m_Choices[0];
+        }
+
+        if (m_Dialog.m_Choices[1] != "")
+        {
+            m_CanvasSecondChoiceButton = Instantiate(m_ChoiceButtonPrefab, transform.position, transform.rotation);
+            m_SecondChoiceButtonUI = GameObject.Find("ChoiceButton");
+            m_SecondChoiceButtonUI.name = "SecondChoiceButton";
+            m_SecondChoiceButtonUI.transform.Translate(m_Decal2);
+            m_SecondChoiceButton = m_SecondChoiceButtonUI.GetComponent<Button>();
+            m_SecondChoiceButton.onClick.AddListener(SecondChoiceDialog);
+
+            GameObject m_SecondChoiceTextUI = GameObject.Find("ChoiceText");
+            m_SecondChoiceTextUI.name = "SecondChoiceText";
+            m_SecondChoiceText = m_SecondChoiceTextUI.GetComponent<Text>();
+            m_SecondChoiceText.text = m_Dialog.m_Choices[1];
+        }
+
+        if (m_Dialog.m_Choices[2] != "")
+        {
+            m_CanvasThirdChoiceButton = Instantiate(m_ChoiceButtonPrefab, transform.position, transform.rotation);
+            m_ThirdChoiceButtonUI = GameObject.Find("ChoiceButton");
+            m_ThirdChoiceButtonUI.name = "ThirdChoiceButton";
+            m_ThirdChoiceButtonUI.transform.Translate(m_Decal3);
+            m_ThirdChoiceButton = m_ThirdChoiceButtonUI.GetComponent<Button>();
+            m_ThirdChoiceButton.onClick.AddListener(ThirdChoiceDialog);
+
+            GameObject m_ThirdChoiceTextUI = GameObject.Find("ChoiceText");
+            m_ThirdChoiceTextUI.name = "ThirdChoiceText";
+            m_ThirdChoiceText = m_ThirdChoiceTextUI.GetComponent<Text>();
+            m_ThirdChoiceText.text = m_Dialog.m_Choices[2];
+        }
+    }
+
+    public void FirstChoiceDialog()
+    {
+        if (m_Dialog.m_NextSentences.Length > 0)
+        {
+            m_Dialog = m_Dialog.m_NextSentences[0];
+            ContinueDialog();
+            HeartHealthVisual.heartHealthSystemStatic.Damage(4);
+        }
+        else
+        {
+            
+            EndDialog();
+        }
+    }
+
+    public void SecondChoiceDialog()
+    {
+        if (m_Dialog.m_NextSentences.Length > 0)
+        {
+            m_Dialog = m_Dialog.m_NextSentences[1];
+            ContinueDialog();
+        }
+        else
+        {
+            EndDialog();
+        }
+    }
+
+    public void ThirdChoiceDialog()
+    {
+        if (m_Dialog.m_NextSentences.Length > 0)
+        {
+            m_Dialog = m_Dialog.m_NextSentences[2];
+            HeartHealthVisual.heartHealthSystemStatic.Heal(4);
+            ContinueDialog();
+        }
+        else
+        {
+            EndDialog();
+        }
+    }
+
+    public void ContinueDialog()
+    {
+        Destroy(m_FirstChoiceButtonUI);
+        Destroy(m_SecondChoiceButtonUI);
+        Destroy(m_ThirdChoiceButtonUI);
+
+        m_DialogText.text = m_Dialog.m_Sentence;
+        if (m_Dialog.m_Choices.Length > 0)
+        {
+            if (m_Dialog.m_Choices[0] != "")
+            {
+                m_CanvasFirstChoiceButton = Instantiate(m_ChoiceButtonPrefab, transform.position, transform.rotation);
+                m_FirstChoiceButtonUI = GameObject.Find("ChoiceButton");
+                m_FirstChoiceButtonUI.name = "FirstChoiceButton";
+                m_FirstChoiceButtonUI.transform.Translate(m_Decal1);
+                m_FirstChoiceButton = m_FirstChoiceButtonUI.GetComponent<Button>();
+                m_FirstChoiceButton.onClick.AddListener(FirstChoiceDialog);
+
+                GameObject m_FirstChoiceTextUI = GameObject.Find("ChoiceText");
+                m_FirstChoiceTextUI.name = "FirstChoiceText";
+                m_FirstChoiceText = m_FirstChoiceTextUI.GetComponent<Text>();
+                m_FirstChoiceText.text = m_Dialog.m_Choices[0];
+            }
+
+            if (m_Dialog.m_Choices[1] != "")
+            {
+                m_CanvasSecondChoiceButton = Instantiate(m_ChoiceButtonPrefab, transform.position + m_Decal2, transform.rotation);
+                m_SecondChoiceButtonUI = GameObject.Find("ChoiceButton");
+                m_SecondChoiceButtonUI.name = "SecondChoiceButton";
+                m_SecondChoiceButtonUI.transform.Translate(m_Decal2);
+                m_SecondChoiceButton = m_SecondChoiceButtonUI.GetComponent<Button>();
+                m_SecondChoiceButton.onClick.AddListener(SecondChoiceDialog);
+
+                GameObject m_SecondChoiceTextUI = GameObject.Find("ChoiceText");
+                m_SecondChoiceTextUI.name = "SecondChoiceText";
+                m_SecondChoiceText = m_SecondChoiceTextUI.GetComponent<Text>();
+                m_SecondChoiceText.text = m_Dialog.m_Choices[1];
+            }
+
+            if (m_Dialog.m_Choices[2] != "")
+            {
+                m_CanvasThirdChoiceButton = Instantiate(m_ChoiceButtonPrefab, transform.position + m_Decal3, transform.rotation);
+                m_ThirdChoiceButtonUI = GameObject.Find("ChoiceButton");
+                m_ThirdChoiceButtonUI.name = "ThirdChoiceButton";
+                m_ThirdChoiceButtonUI.transform.Translate(m_Decal3);
+                m_ThirdChoiceButton = m_ThirdChoiceButtonUI.GetComponent<Button>();
+                m_ThirdChoiceButton.onClick.AddListener(ThirdChoiceDialog);
+
+                GameObject m_ThirdChoiceTextUI = GameObject.Find("ChoiceText");
+                m_ThirdChoiceTextUI.name = "ThirdChoiceText";
+                m_ThirdChoiceText = m_ThirdChoiceTextUI.GetComponent<Text>();
+                m_ThirdChoiceText.text = m_Dialog.m_Choices[2];
+            }
+        }
+        else
+        {
+            m_CanvasFinishDialogButton = Instantiate(m_ChoiceButtonPrefab, transform.position, transform.rotation);
+            m_FinishDialogButtonUI = GameObject.Find("ChoiceButton");
+            m_FinishDialogButtonUI.name = "FinishDialogButton";
+            m_FinishDialogButtonUI.transform.Translate(m_DecalF);
+            m_FinishDialogButton = m_FinishDialogButtonUI.GetComponent<Button>();
+            m_FinishDialogButton.onClick.AddListener(EndDialog);
+
+            GameObject m_FinishDialogTextUI = GameObject.Find("ChoiceText");
+            m_FinishDialogTextUI.name = "ThirdChoiceText";
+            m_FinishDialogText = m_FinishDialogTextUI.GetComponent<Text>();
+            m_FinishDialogText.text = "Fin";
+        }
+    }
+
+    public void EndDialog()
+    {
+        Destroy(m_CanvasFirstChoiceButton);
+        Destroy(m_CanvasSecondChoiceButton);
+        Destroy(m_CanvasThirdChoiceButton);
+        Destroy(m_CanvasFinishDialogButton);
+        Destroy(m_DialogBox);
+        GetComponent<NPC>().SetEndDialog();
+    }
+}
---
+++ Projet_DGÀSF/Assets/Scripts/HeartHealthVisual.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HeartHealthVisual : MonoBehaviour
+{
+    public static HeartHealthSystem heartHealthSystemStatic;
+    [SerializeField] private Sprite heart0Sprite;
+    [SerializeField] private Sprite heart1Sprite;
+    [SerializeField] private Sprite heart2Sprite;
+    [SerializeField] private Sprite heart3Sprite;
+    [SerializeField] private Sprite heart4Sprite;
+
+    private List<HeartImage> HeartImageList;
+    private HeartHealthSystem heartHealthSystem;
+
+    private void Awake()
+    {
+        HeartImageList = new List<HeartImage>();
+    }
+
+    private void Start()
+    {
+        HeartHealthSystem heartHealthSystem = new HeartHealthSystem(40);
+        SetHeartHealthSystem(heartHealthSystem);
+
+        
+    }
+
+    public void SetHeartHealthSystem(HeartHealthSystem heartHealthSystem)
+    {
+        this.heartHealthSystem = heartHealthSystem;
+        heartHealthSystemStatic = heartHealthSystem;
+
+        List<HeartHealthSystem.Heart> heartList = heartHealthSystem.GetHeartList();
+        Vector2 heartAnchoredPosition = new Vector2(-350, 100);
+
+        for (int i = 0; i < heartList.Count; i++)
+        {
+            HeartHealthSystem.Heart heart = heartList[i];
+            CreateHeartImage(heartAnchoredPosition).SetHeartFragments(heart.GetFragmentAmount());
+            heartAnchoredPosition += new Vector2(20, 0);
+            if (i == 9 || i == 19 || i == 29)
+            {
+                
+                heartAnchoredPosition += new Vector2(-200, -40*( (i/10)+1   ));
+                Debug.Log("i = " + i + " ; (i/10)+1 =  "+ ((i / 10) + 1));
+            }
+        }
+        heartHealthSystem.OnDamaged += heartHealthSystem_OnDamaged;
+        heartHealthSystem.OnHealed += heartHealthSystem_OnHealed;
+        heartHealthSystem.OnDead += heartHealthSystem_OnDead;
+        //heartHealthSystem.Damage(2);
+        //heartHealthSystem.Heal(1);
+        heartHealthSystemStatic.Damage(4*35);
+        
+
+    }
+
+    private void heartHealthSystem_OnDamaged(object sender, System.EventArgs e)
+    {
+        RefreshAllHearts();
+    }
+    private void heartHealthSystem_OnDead(object sender, System.EventArgs e)
+    {
+        //CMDebug.TextPopupMouse("Dead !");
+    }
+    private void heartHealthSystem_OnHealed(object sender, System.EventArgs e)
+    {
+        RefreshAllHearts();
+    }
+
+    private void RefreshAllHearts()
+    {
+        List<HeartHealthSystem.Heart> heartList = heartHealthSystem.GetHeartList();
+        for (int i = 0; i < HeartImageList.Count; i++)
+        {
+            HeartImage heartImage = HeartImageList[i];
+            HeartHealthSystem.Heart heart = heartList[i];
+            heartImage.SetHeartFragments(heart.GetFragmentAmount());
+
+        }
+    }
+    /*
+    private void HealMainFunction(List<HeartHealthSystem.Heart> heartList, Vector2 heartAnchoredPosition,int HealAmount)
+    {
+        int newHearts = heartHealthSystem.Heal(HealAmount);
+        Debug.Log("newHearts = " + newHearts);
+        for (int i = heartList.Count - newHearts; i < heartList.Count; i++)
+        {
+            HeartHealthSystem.Heart heart = heartList[i];
+            CreateHeartImage(heartAnchoredPosition).SetHeartFragments(heart.GetFragmentAmount());
+            heartAnchoredPosition += new Vector2(30, 0);
+            Debug.Log("i = " + i);
+            if (i == 9 || i == 19 || i == 29)
+            {
+                heartAnchoredPosition = new Vector2(0, -70 * ((i / 10) + 1));
+                Debug.Log("i = " + i + " ; (i/10)+1 =  " + ((i / 10) + 1));
+            }
+        }
+    }*/
+
+    private HeartImage CreateHeartImage( Vector2 anchoredPosition)
+    {
+        GameObject heartGameObject = new GameObject("Heart", typeof(Image));
+        heartGameObject.transform.parent = transform;
+        heartGameObject.transform.localPosition = Vector3.zero;
+
+        heartGameObject.GetComponent<RectTransform>().anchoredPosition = anchoredPosition;
+        heartGameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(45, 45);
+
+        Image heartImageUI = heartGameObject.GetComponent<Image>();
+        heartImageUI.sprite = heart1Sprite;
+
+        HeartImage heartImage = new HeartImage(this,heartImageUI);
+        HeartImageList.Add(heartImage);
+
+        return heartImage;
+    }
+
+    public class HeartImage
+    {
+        private Image heartImage;
+        private HeartHealthVisual heartHealthVisual;
+        public HeartImage(HeartHealthVisual heartHealthVisual, Image heartImage)
+        {
+            this.heartImage = heartImage;
+            this.heartHealthVisual = heartHealthVisual;
+        }
+        public void SetHeartFragments(int fragments)
+        {
+            switch (fragments)
+            {
+                case 0: heartImage.enabled  = false; break;
+                case 1: heartImage.enabled = true; heartImage.sprite = heartHealthVisual.heart1Sprite; break;
+                case 2: heartImage.enabled = true; heartImage.sprite = heartHealthVisual.heart2Sprite; break;
+                case 3: heartImage.enabled = true; heartImage.sprite = heartHealthVisual.heart3Sprite; break;
+                case 4: heartImage.enabled = true; heartImage.sprite = heartHealthVisual.heart4Sprite; break;
+
+
+                //case 0: heartImage.sprite = heartHealthVisual.heart0Sprite; break;
+            }
+        }
+    }
+}
