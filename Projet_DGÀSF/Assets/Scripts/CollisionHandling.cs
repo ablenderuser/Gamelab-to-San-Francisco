ec27b6981ff88c75adf6dba66e4807b7c2d5dff5
Fourka Bilal <fourka12@gmail.com>
2020-03-31 16:19:36 +0200

    collision Handling

---
+++ Projet_DGÀSF/Assets/Scripts/CollisionHandling.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandling : MonoBehaviour
+{
+
+    private bool INzone = false;
+    private SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {	
+    	if(INzone==false){
+    		sprite.sortingOrder = 2;
+	        INzone =true;
+	        Vector3 newPosition = transform.position;
+	        newPosition.z = transform.position.z+0.5f;
+	        transform.position = newPosition;
+    	}
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        sprite.sortingOrder = 0;
+        Vector3 newPosition = transform.position;
+        newPosition.z = transform.position.z-0.5f;
+        transform.position = newPosition;
+        INzone =false;
+    }
+}
