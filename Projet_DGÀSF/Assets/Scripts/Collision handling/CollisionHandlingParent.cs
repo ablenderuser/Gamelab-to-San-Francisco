fecb2d8901fdd85ab8043ef253035a5b3ce90d3c
root <root@Sylvain-GEISER.localdomain>
2020-05-13 09:53:26 +0200

    Intégration du niveau 2 et explications inter-niveaux

--- Projet_DGÀSF/Assets/Scripts/Collision handling/CollisionHandlingParent.cs
+++ Projet_DGÀSF/Assets/Scripts/Collision handling/CollisionHandlingParent.cs
@@ -26,6 +26,7 @@ public class CollisionHandlingParent : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Entré");
         if (!INzone)
         {
             INzone = true;
@@ -38,6 +39,7 @@ public class CollisionHandlingParent : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        Debug.Log("Sorti");
         sprite.sortingOrder = 3;
         Vector3 newPosition = transform.parent.position;
         newPosition.z = transform.parent.position.z - 0.5f;
